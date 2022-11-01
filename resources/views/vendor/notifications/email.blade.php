ffcb4ed6e23dd6b26d69ccaacd5519c08e92cf38
Andrew Judd <andrew@judd.dev>
2022-10-31 20:06:34 -0400

    chore(notifications): notification resources
    Adding in notification resources

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -43,20 +43,16 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-@lang('Regards'),<br>{{ setting('app_name', config('app.name')) }}
+{{ trans('notifications.common.salutation') }}<br>{{ setting('app_name', config('app.name')) }}
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-@lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: [:actionURL](:actionURL)',
-    [
+    {{ trans('notifications.common.alternative_link', [
         'actionText' => $actionText,
         'actionURL' => $actionUrl,
-    ]
-)
+    ]) }}
 @endcomponent
 @endisset
 @endcomponent
